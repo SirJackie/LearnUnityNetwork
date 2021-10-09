e61833036f54093728c34dfde6873d2a2298aa98
lyj00912 <3122742760@qq.com>
2021-10-09 22:29:43 +0800

    refactored the code, added chinese-labeled buttons

--- LearnUnityNetwork/Assets/NetworkTest.cs
+++ LearnUnityNetwork/Assets/NetworkTest.cs
@@ -4,47 +4,44 @@ using UnityEngine;
 
 public class NetworkTest : MonoBehaviour {
 
-	int myPort = 16543;//端口号
-	string serverIP = "192.168.3.2";//服务器ip
-	string yourState = "";
-	// Use this for initialization
-	void Start () {
-
-	}
-
-	// Update is called once per frame
-	void Update () {
-
-	}
+	string ip = "192.168.3.2";
+	string port = "16543";
 
 	void OnGUI() {
-		if (GUILayout.Button("Creat Server")) {
-			/*创建一个服务器
-             * 3个参数
-             * 1、允许的入站连接或玩家数量
-             * 2、监听的端口
-             * 3、设置穿透功能
-             */
-			Network.InitializeServer(32, myPort,false);
-		}
-		if (GUILayout.Button("Connect Server")) {
-			/*客户端连接服务器*/
-			Network.Connect(serverIP, myPort);
-		}
-		GUILayout.TextArea(yourState);
-		switch (Network.peerType) { 
-		case NetworkPeerType.Server:
-			yourState = "You are Server, The server has Created!";
-			GUILayout.TextField("Had "+Network.connections.Length.ToString()+" Connected");
-			break;
-		case NetworkPeerType.Client:
-			yourState = "You are Client";
-			break;
-		case NetworkPeerType.Connecting:
-			yourState = "Connecting...";
-			break;
+
+		switch (Network.peerType) {
 		case NetworkPeerType.Disconnected:
-			yourState = "You had not connected the server";
+			GUILayout.Label ("尚未建立任何连接。");
+
+			ip = GUILayout.TextField (ip);
+			port = GUILayout.TextField (port);
+
+			if (GUILayout.Button ("连接")) {
+				Network.Connect (ip, int.Parse (port));
+			}
+
+			if (GUILayout.Button ("使用上述端口开设服务器")) {
+				Network.InitializeServer (32, int.Parse (port), false);
+			}
+			break;
+
+		case NetworkPeerType.Connecting:
+			GUILayout.Label ("正在连接...");
+			break;
+		
+		case NetworkPeerType.Server:
+			GUILayout.Label ("服务器已开设。地址：" + ip + ":" + port);
+			GUILayout.Label ("有" + Network.connections.Length.ToString () + "个客户端与这个服务器建立了连接。");
+			if (GUILayout.Button ("断开连接/终止服务器")) {
+				Network.Disconnect ();
+			}
+			break;
+
+		case NetworkPeerType.Client:
+			GUILayout.TextField ("已连接。");
+			if (GUILayout.Button ("断开连接/终止服务器")) {
+				Network.Disconnect ();
+			}
 			break;
 		}
 	}
